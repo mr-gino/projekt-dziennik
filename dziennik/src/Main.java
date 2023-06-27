b8753088b510455d2874b2812b2045a235ec973d
mr-gino <skocz.dawid@o2.pl>
2023-06-27 23:18:19 +0200

    Dodaje wywolanie metody Gui

--- dziennik/src/Main.java
+++ dziennik/src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         LoginSystem loginSystem = new LoginSystem();
         loginSystem.loadUsersFromFile("users.txt");
+
+        Gui gui = new Gui(loginSystem);
+        gui.start();
     }
 }
